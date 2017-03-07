355e6b0acc8c0c64609d2830d295dc94834944dd
GloamingCat <nightlywhiskers@gmail.com>
2017-03-07 08:24:22 -0300

    Matrix2, Matrix3

---
+++ Game/scripts/core/algorithm/Matrix2.lua
@@ -0,0 +1,55 @@
+
+local insert = table.insert
+local remove = table.remove
+
+--[[===========================================================================
+
+A 2D matrix of fixed size.
+
+=============================================================================]]
+
+local Matrix2 = require('core/class'):new()
+
+function Matrix2:init(width, height, startValue)
+  self.width = width
+  self.height = height
+  for i = 1, width * height do
+    self[i] = startValue
+  end
+end
+
+function Matrix2:get(i, j)
+  return self[(j - 1) * self.width + i]
+end
+
+function Matrix2:set(value, i, j)
+  self[(j - 1) * self.width + i] = value
+end
+
+function Matrix2:iterator()
+  local i = 0
+  local size = #self
+  return function()
+    i = i + 1
+    while self[i] == nil and i <= size do
+      i = i + 1
+    end
+    return self[i]
+  end
+end
+
+-- Converting to string.
+-- @ret(string) A string representation
+function Matrix2:toString()
+  local s = '{ '
+  for i = 1, self.width do
+    s = s .. '{ '
+    for j = 1, self.height do
+      s = s .. self:get(i, j, k) .. ' '
+    end
+    s = s .. '}'
+  end
+  s = s .. ' }'
+end
+
+return Matrix2
