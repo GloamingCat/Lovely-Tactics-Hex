e7806b7fb9a2ec0d565f95ecd2698225a30729c2
GloamingCat <nightlywhiskers@gmail.com>
2022-09-22 08:54:26 -0300

    LoadBattle test script

---
+++ scripts/custom/test/LoadBattle.lua
@@ -0,0 +1,33 @@
+
+--[[===============================================================================================
+
+LoadBattle
+---------------------------------------------------------------------------------------------------
+Test script that starts battle when field is loaded.
+
+=================================================================================================]]
+
+return function(script)
+    
+  script:addEvent(function()
+    script:addMember { key = 'Test' }
+    script:hideMember { key = 'Arthur' }
+  end)
+
+  script:addEvent(script.startBattle, nil, {
+    intro = true,
+    escapeEnabled = true,
+    gameOverCondition = script.args.gameOverCondition or 'survive',
+    fieldID = 7,
+    fade = 60
+  })
+
+  script:addEvent(script.finishBattle, nil, { fade = 60, wait = true })
+  
+  script:addEvent(function()
+    script:hideMember { key = 'Test' }
+    script:addMember { key = 'Arthur' }
+    FieldManager.hud:show()
+  end)
+
+end
